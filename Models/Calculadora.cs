5a5090aaeeb149fe12a0c5196095898f1308825f
CamilaAlbieri <camilamattos.mila@gmail.com>
2025-08-26 16:35:33 -0300

    Operadores Aritméticos e a classe Math

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -11,5 +11,54 @@ namespace Fundamentos.Models
         {
             Console.WriteLine($"Soma de {x} + {y} = {x + y}");
         }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"Subtração de {x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"Multiplicação de {x} * {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"Divisão de {x} / {y} = {x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"Potência de {x} elevado a {y} = {pot}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * (Math.PI) / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(seno, 4)}");
+        }
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * (Math.PI) / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo} = {Math.Round(coseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * (Math.PI) / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo} = {Math.Round(tangente, 4)}");
+        }
+
+        public void RaizQuadrada(double numero)
+        {
+            double raiz = Math.Sqrt(numero);
+            Console.WriteLine($"Raiz Quadrada de {numero} = {Math.Round(raiz, 4)}");
+        }
+        
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,66 +2,23 @@
 
 //Operadores Aritméticos
 
+Calculadora calc = new Calculadora();
+
+// calc.Somar(10,20);
+// calc.Subtrair(13, 20);
+// calc.Multiplicar(4, 15);
+// calc.Dividir(100, 20);
+// calc.Potencia(3, 4);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+calc.RaizQuadrada(9);
+
+// int numeroIncremento = 10;
+
+// Console.WriteLine($"Incrementando o valor de {numeroIncremento} em 1: {++numeroIncremento}");
 
 
+// int numeroDecremento = 10;
+// Console.WriteLine($"Decrementando o valor de {numeroDecremento} em 1: {--numeroDecremento}");
 
-
-
-//------------------------------------------------
-//Switch Case
-
-// Console.WriteLine("Digite uma letra: ");
-
-// string letra = Console.ReadLine();
-
-// switch (letra)
-// {
-//     case "a":
-//     case "e":
-//     case "i":
-//     case "o":
-//     case "u":
-//         Console.WriteLine("A letra digitada é uma vogal.");
-//         break;
-    
-//     default:
-//         Console.WriteLine("A letra digitada é uma consoante.");
-//         break;
-
-// }
-
-
-
-
-//------------------------------------------------
-//Operadores Condicionais
-
-// int quantidadeEmEstoque = 10;
-// int quantidadeCompra = 0;
-// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
-// Console.WriteLine($"Quantidade em estoque:  {quantidadeEmEstoque}");
-// Console.WriteLine($"Quantidade da compra:  {quantidadeCompra} ");
-// Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
-
-// if (quantidadeCompra == 0)
-// {
-//     Console.WriteLine("Venda Inválida, quantidade de compra é zero.");
-// }
-// else if (possivelVenda)
-// {
-//     Console.WriteLine("Compra realizada com sucesso!");
-// }
-// else
-// {
-//     Console.WriteLine("Desculpe, não temos estoque suficiente.");
-// }
-
-//------------------------------------------------
-
-//Cast implícito
-
-//não precisa de conversão explícita, pois ele faz automaticamente
-// int a = 5;
-// double b = a; // int para double
-
-// Console.WriteLine(b); 
